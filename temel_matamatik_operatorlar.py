36dfb4e7550493cce3521c550560f3017be9ff93
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-09-24 18:20:12 +0400

    temel matematik operatörleri

---
+++ temel_matamatik_operatorlar.py
@@ -0,0 +1,17 @@
+# Temel mayamaktik operatorlari
+
+# toplama
+toplama=5+4
+# cikarma
+cikarma=5-4
+# carpma
+carpma=5*4
+# bolme
+bolme=5/4
+# kalisiz bolme
+kalisiz_bolme=5//4
+# kalani bulma
+kalan=5%4
+# us alma
+us_al=5**4 #( 5 uzeri 4)
+print(toplama,cikarma,carpma,bolme,kalisiz_bolme,kalan,us_al)
