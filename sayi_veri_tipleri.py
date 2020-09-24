f0950b3232f01197be1eb0873796b1e462dbe2fc
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-09-24 17:21:14 +0400

    temel bilgiler

---
+++ sayi_veri_tipleri.py
@@ -0,0 +1,14 @@
+# Pythonda sayi veri tipleri
+# a,b,c bir degiskendir(istedigimiz ismi verebiliriz)
+# a,b,c degiskeni sayi veya metin degerlerini alabilir
+
+a=3 # int (integer)
+a+=1 # a'nin degerini 1 artirir
+
+b=3.8 # float
+a,b=b,a # a'nin degerini b'ye b'nin degerini a'ya atiyyor
+
+
+
+
+
