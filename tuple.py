d7022503cc75e7f4e9ca5142c3bf063a9557e91c
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-09-29 01:27:43 +0400

    demetler

---
+++ tuple.py
@@ -0,0 +1,14 @@
+# TUPLE VERI TIPI
+
+
+# bos demet olusturmak
+demet=()
+demet=tuple()
+
+demet=(1,2,3,4,5)
+# demetler indexlenebilir , parcalanabilir, ancak degistirilemez
+demet[3]
+demet[: : -1]
+
+demet.count(3) # 3 elemani bu demetde kac defa gecti
+demet.index(2) # 2 elemani bu demetde kacinci indexte
