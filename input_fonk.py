645ef2f571beed798700a980148242b9d8d4fe5a
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-09-29 02:00:29 +0400

    Input() font

---
+++ input_fonk.py
@@ -0,0 +1,6 @@
+# input() fonksiyonu kullanicidan deger almak icindir,
+# kullanicidan alinan deger string veri tipinde olur
+
+# integere ceviriyoruz
+a=int(input("Lutfen bir sayi giriniz"))
+print(a)
