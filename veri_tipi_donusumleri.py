d10d0d6204dfbe41881a18e2d7781f76e35c4a08
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-09-28 22:13:58 +0400

    veri tipi dönüşümleri

---
+++ veri_tipi_donusumleri.py
@@ -0,0 +1,20 @@
+# veri tipi donusumleri
+
+# tam sayiyi ondalikli sayiyi cevirme
+a=3
+b=float(a)
+
+# tam sayiyi metin degerine cevirme
+
+a=345
+d=str(a)
+
+# ondalikli degeri tam degere cevirme
+
+a=3.45
+c=int(a)
+
+# Stringimizi integer degere cevirmek icin stringimizin sadece sayilardan olusmasi gerekmektedir
+
+a="4663627"
+g=int(a)
